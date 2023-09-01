bee1a49cea782dfaad775415d5d961b951f2159b
carolina Ramirez <carocoterio123@gmail.com>
2023-09-01 16:21:19 -0500

    En esta rama realice la clase proyecto.cs

---
+++ parcial.shared/Entities/proyectos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial.shared.Entities
+{
+    public class proyecto
+    {
+     
+     public int Id { get; set; }
+
+        
+     public string NombreProyecto { get; set; } = null;
+     public string FechaInicio { get; set; } = null;
+     public string FechaFin { get; set; } = null;
+     public string liderProyecto { get; set; } = null;
+     public string Descripcion { get; set; } = null;
+     public string AreaInvestigacion { get; set; } = null;
+
+    }
+}
