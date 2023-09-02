e807c9d23b09c3469e18052cc7ae98f1a49d51c3
carolina Ramirez <carocoterio123@gmail.com>
2023-09-02 11:53:23 -0500

    se realizo la migracion y los controladores

---
+++ parcial.API/Controllers/proyectosController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using parcial.API.Data;
+using parcial.shared.Entities;
+
+namespace parcial.API.Controllers
+{
+    [ApiController]
+    [Route("api/proyectos")]    
+    public class proyectosController : ControllerBase { 
+
+     private readonly DataContext _context;
+
+     public proyectosController(DataContext context) 
+        {
+        
+        _context = context;
+        }
+       
+        //get por parametro
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+
+        {
+
+            await _context.proyectos.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok();
+
+        }
+        //get por lista
+        [HttpGet]
+        public async Task<ActionResult> Get()
+
+        {
+
+            return Ok(await _context.proyectos.ToListAsync());
+
+        }
+        // insertar un registro
+        [HttpPost]
+
+        public async Task<ActionResult> post(proyecto proyecto)
+
+        {
+
+            _context.Add(proyecto);
+            await _context.SaveChangesAsync();
+            return Ok(proyecto);
+        }
+
+
+    }
+}
+
+ 
---
+++ parcial.API/Migrations/20230902025120_Inicial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace parcial.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Investigadors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AfiliacionInstitucional = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investigadors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "proyectos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreProyecto = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FechaInicio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaFin = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    liderProyecto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AreaInvestigacion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_proyectos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_proyectos_NombreProyecto",
+                table: "proyectos",
+                column: "NombreProyecto",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Investigadors");
+
+            migrationBuilder.DropTable(
+                name: "proyectos");
+        }
+    }
+}
