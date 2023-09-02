b682e74e1752c8375fe0f8df01a2a2cdfe4d7ea9
carolina Ramirez <carocoterio123@gmail.com>
2023-09-01 19:06:54 -0500

    se crearon las clases investigador y Datacontext

---
+++ parcial.API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using parcial.shared.Entities;
+
+namespace parcial.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options): base(options) { }
+
+        public DbSet<proyecto>proyectos { get; set; }
+        public DbSet<investigador>Investigadors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<proyecto>().HasIndex(c => c.NombreProyecto).IsUnique();
+
+        }
+
+    }
+}
---
+++ parcial.shared/Entities/investigador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial.shared.Entities
+{
+    public class investigador
+    {
+      public int Id { get; set; }
+     public string Nombre { get; set; } = null;
+     public string AfiliacionInstitucional { get; set; } = null;
+     public string Especializacion = null;
+     public string Rol { get; set; } = null;
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
